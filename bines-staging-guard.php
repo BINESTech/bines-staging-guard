1d3d86a9c1caa4cccb330b108748b2b579788927
Aeros <aerossalaga@gmail.com>
2026-09-06 02:33:32 +0800

    chore: license the plugin under GPL-2.0-or-later

--- bines-staging-guard.php
+++ bines-staging-guard.php
@@ -5,6 +5,8 @@
  * Version: 0.1.0
  * Requires PHP: 8.3
  * Author: BINESTech
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package BinesGuard
  */
